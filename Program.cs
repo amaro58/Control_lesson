2c04c162ae9df3042df5d854190730279d75b24b
Anastasiya Maro <anastasiya.maro@mail.ru>
2024-04-30 12:37:57 +0300

    Добавили функцию ввода данных с консоли

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿﻿string[] workArray = FillArray();
+
+string[] FillArray()
+{
+    Console.WriteLine("Введите данные через пробел, по окончании ввода нажмите Enter: ");
+    string? enterSymbols = Console.ReadLine();
+    if (enterSymbols == null) { enterSymbols = ""; };
+    char[] separators = new char[] { ',', ' ' };
+    string[] workArray = enterSymbols.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+    return workArray;
+}
